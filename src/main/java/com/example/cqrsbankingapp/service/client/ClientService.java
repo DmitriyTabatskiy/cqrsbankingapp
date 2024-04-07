07b4fbb8b5fbf00ed7b669c553304890228abeb7
Tabatskiy <coldfire347@gmail.com>
2024-04-07 11:41:01 +0300

    service level implementation

---
+++ src/main/java/com/example/cqrsbankingapp/service/client/ClientService.java
@@ -0,0 +1,11 @@
+package com.example.cqrsbankingapp.service.client;
+
+import com.example.cqrsbankingapp.domain.model.Client;
+import com.example.cqrsbankingapp.service.CommandService;
+import com.example.cqrsbankingapp.service.QueryService;
+
+public interface ClientService
+
+        extends QueryService<Client>, CommandService<Client> {
+
+}
